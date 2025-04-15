d8440668f0964b5d7b8bfb74de5b0c134ee5aafd
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2025-04-15 19:37:54 +0800

    feat: 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages";
+import NotFoundPage from "./pages/404";
 import AboutPage from "./pages/about";
 import ContactPage from "./pages/contact";
 import RegisterPage from "./pages/register";
@@ -21,6 +22,7 @@ function App() {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </main>
       {shouldHideNavbar && <Footer />}
